e51115afd84cf8575101dab9605adbc8e703abcd
joao4018 <joao4018@gmail.com>
2018-03-22 10:30:31 -0300

    Included the pixel struct inside the image struc

--- main.c
+++ main.c
@@ -10,7 +10,8 @@ typedef struct _pixel {
 } Pixel;
 
 typedef struct _image {
-  unsigned short int pixel[512][512][3];
+
+  Pixel pixel[512][512];
   unsigned int widht;
   unsigned int height;
 } Image;
@@ -45,7 +46,7 @@ int main(){
       case 1: { // Escala de Cinza
         image = change_color_to_gray(image);
         break;
-      }
+       }
       case 2: { // Filtro Sepia
         image = filter_serpia(image);
         break;
@@ -84,16 +85,16 @@ int max(int a, int b) {
 }
 
 int calculate_average(Image *image){
-  int media = ((*image).pixel[counter][counter_two][0] +
-  (*image).pixel[counter][counter_two][1] +
-  (*image).pixel[counter][counter_two][2])/3;
+  int media = ((*image).pixel[counter][counter_two].red +
+  (*image).pixel[counter][counter_two].green +
+  (*image).pixel[counter][counter_two].blue)/3;
 return media;
 }
 
 void copy_data(Image *image,int *media){
-  (*image).pixel[counter][counter_two][0] = *media;
-  (*image).pixel[counter][counter_two][1] = *media;
-  (*image).pixel[counter][counter_two][2] = *media;
+  (*image).pixel[counter][counter_two].red = *media;
+  (*image).pixel[counter][counter_two].green = *media;
+  (*image).pixel[counter][counter_two].blue = *media;
 }
 
 
@@ -107,15 +108,15 @@ Image change_color_to_gray(Image image) {
   }
     return image;
 }
-
+//
 void change_pixel_color(Image *image, Pixel *media, int *tamanho){
   int menor_height = max(((*image).height - 1), (counter + *tamanho/2));
   int min_widht = max(((*image).widht - 1), (counter_two + *tamanho/2));
   for(int x = (0 > counter - *tamanho/2 ? 0 : counter - *tamanho/2); x <= menor_height; ++x) {
     for(int y = (0 > counter_two - *tamanho/2 ? 0 : counter_two - *tamanho/2); y <= min_widht; ++y) {
-      (*media).red += (*image).pixel[x][y][0];
-      (*media).green += (*image).pixel[x][y][1];
-      (*media).blue += (*image).pixel[x][y][2];
+      (*media).red += (*image).pixel[x][y].red;
+      (*media).green += (*image).pixel[x][y].green;
+      (*media).blue += (*image).pixel[x][y].blue;
     }
   }
 }
@@ -125,9 +126,9 @@ void change_pixel_image_color(Image *image, Pixel *media, int *tamanho){
   (*media).green /= *tamanho * *tamanho;
   (*media).blue /= *tamanho * *tamanho;
 
-  (*image).pixel[counter][counter_two][0] = (*media).red;
-  (*image).pixel[counter][counter_two][1] = (*media).green;
-  (*image).pixel[counter][counter_two][2] = (*media).blue;
+  (*image).pixel[counter][counter_two].red = (*media).red;
+  (*image).pixel[counter][counter_two].green = (*media).green;
+  (*image).pixel[counter][counter_two].blue = (*media).blue;
 }
 
 Image blur(Image *image) {
@@ -140,7 +141,7 @@ Image blur(Image *image) {
       change_pixel_image_color(*(&image), &media, &tamanho);
     }
   }
-`return *image;
+  return *image;
 }
 
 Image rotacionar90direita(Image image) {
@@ -155,9 +156,9 @@ Image rotacionar90direita(Image image) {
 
     for (unsigned int i = 0, y = 0; i < rotacionada.height; ++i, ++y) {
       for (int j = rotacionada.widht - 1, x = 0; j >= 0; --j, ++x) {
-        rotacionada.pixel[i][j][0] = image.pixel[x][y][0];
-        rotacionada.pixel[i][j][1] = image.pixel[x][y][1];
-        rotacionada.pixel[i][j][2] = image.pixel[x][y][2];
+        rotacionada.pixel[i][j].red = image.pixel[x][y].red;
+        rotacionada.pixel[i][j].green  = image.pixel[x][y].green;
+        rotacionada.pixel[i][j].blue = image.pixel[x][y].blue;
       }
     }
   }
@@ -167,9 +168,9 @@ Image rotacionar90direita(Image image) {
 Image inverter_cores(Image image) {
   for (counter = 0; counter < image.height; ++counter) {
     for (counter_two = 0; counter_two < image.widht; ++counter_two) {
-      image.pixel[counter][counter_two][0] = 255 - image.pixel[counter][counter_two][0];
-      image.pixel[counter][counter_two][1] = 255 - image.pixel[counter][counter_two][1];
-      image.pixel[counter][counter_two][2] = 255 - image.pixel[counter][counter_two][2];
+      image.pixel[counter][counter_two].red = 255 - image.pixel[counter][counter_two].red;
+      image.pixel[counter][counter_two].green = 255 - image.pixel[counter][counter_two].green;
+      image.pixel[counter][counter_two].blue = 255 - image.pixel[counter][counter_two].blue;
     }
   }
   return image;
@@ -188,9 +189,9 @@ Image cortar_imagem(Image img) {
 
   for(int i = 0; i < h; ++i) {
     for(int j = 0; j < w; ++j) {
-      cortada.pixel[i][j][0] = img.pixel[i + y][j + x][0];
-      cortada.pixel[i][j][1] = img.pixel[i + y][j + x][1];
-      cortada.pixel[i][j][2] = img.pixel[i + y][j + x][2];
+      cortada.pixel[i][j].red = img.pixel[i + y][j + x].red;
+      cortada.pixel[i][j].green = img.pixel[i + y][j + x].green;
+      cortada.pixel[i][j].blue = img.pixel[i + y][j + x].blue;
     }
   }
 
@@ -209,9 +210,9 @@ Image read_image() {
   for (counter = 0; counter < image.height; ++counter) {
     for (counter_two = 0; counter_two < image.widht; ++counter_two) {
 
-      scanf("%hu", &image.pixel[counter][counter_two][0]);
-      scanf("%hu",&image.pixel[counter][counter_two][1]);
-      scanf("%hu",&image.pixel[counter][counter_two][2]);
+      scanf("%hu", &image.pixel[counter][counter_two].red);
+      scanf("%hu",&image.pixel[counter][counter_two].green);
+      scanf("%hu",&image.pixel[counter][counter_two].blue);
     }
   }
   return image;
@@ -223,10 +224,10 @@ void print_image(Image image){
 
   for (counter = 0; counter < image.height; ++counter) {
     for (counter_two = 0; counter_two < image.widht; ++counter_two) {
-      printf("%hu %hu %hu ", image.pixel[counter][counter_two][0],
+      printf("%hu %hu %hu ", image.pixel[counter][counter_two].red,
 
-        image.pixel[counter][counter_two][1],
-        image.pixel[counter][counter_two][2]);
+        image.pixel[counter][counter_two].green,
+        image.pixel[counter][counter_two].blue);
 
       }
     printf("\n");
@@ -250,17 +251,17 @@ Image mirror(Image image){
       else x = image.height - 1 - i2;
 
       Pixel aux1;
-      aux1.red = image.pixel[i2][j][0];
-      aux1.green = image.pixel[i2][j][1];
-      aux1.blue = image.pixel[i2][j][2];
+      aux1.red = image.pixel[i2][j].red;
+      aux1.green = image.pixel[i2][j].green;
+      aux1.blue = image.pixel[i2][j].blue;
 
-      image.pixel[i2][j][0] = image.pixel[x][y][0];
-      image.pixel[i2][j][1] = image.pixel[x][y][1];
-      image.pixel[i2][j][2] = image.pixel[x][y][2];
+      image.pixel[i2][j].red = image.pixel[x][y].red;
+      image.pixel[i2][j].green = image.pixel[x][y].green;
+      image.pixel[i2][j].blue = image.pixel[x][y].blue;
 
-      image.pixel[x][y][0] = aux1.red;
-      image.pixel[x][y][1] = aux1.green;
-      image.pixel[x][y][2] = aux1.blue;
+      image.pixel[x][y].red = aux1.red;
+      image.pixel[x][y].green = aux1.green;
+      image.pixel[x][y].blue = aux1.blue;
     }
   }
   return image;
@@ -270,21 +271,21 @@ Image filter_serpia(Image image){
   for (counter = 0; counter < image.height; ++counter) {
     for (counter_two = 0; counter_two < image.widht; ++counter_two) {
       unsigned short int pixel[3];
-      pixel[0] = image.pixel[counter][counter_two][0];
-      pixel[1] = image.pixel[counter][counter_two][1];
-      pixel[2] = image.pixel[counter][counter_two][2];
+      pixel[0] = image.pixel[counter][counter_two].red;
+      pixel[1] = image.pixel[counter][counter_two].green;
+      pixel[2] = image.pixel[counter][counter_two].blue;
 
       int p =  pixel[0] * .393 + pixel[1] * .769 + pixel[2] * .189;
       int menor_r = (255 >  p) ? p : 255;
-      image.pixel[counter][counter_two][0] = menor_r;
+      image.pixel[counter][counter_two].red = menor_r;
 
       p =  pixel[0] * .349 + pixel[1] * .686 + pixel[2] * .168;
       menor_r = (255 >  p) ? p : 255;
-      image.pixel[counter][counter_two][1] = menor_r;
+      image.pixel[counter][counter_two].green = menor_r;
 
       p =  pixel[0] * .272 + pixel[1] * .534 + pixel[2] * .131;
       menor_r = (255 >  p) ? p : 255;
-      image.pixel[counter][counter_two][2] = menor_r;
+      image.pixel[counter][counter_two].blue = menor_r;
     }
   }
 return image;
